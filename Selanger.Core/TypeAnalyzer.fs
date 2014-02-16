c9333006c324207b7543bb914d763661cf85aa56
David Alpert <david@spinthemoose.com>
2014-02-15 22:30:36 -0600

    initial draft of namespace analysis

---
+++ Selanger.Core/TypeAnalyzer.fs
@@ -0,0 +1,33 @@
+﻿namespace Selanger.Core
+
+open System
+open System.Linq
+open System.IO
+open System.Reflection
+
+type TypeSummary(file:FileInfo, t:Type) =
+    member this.Name = t.Name
+    member this.Namespace = t.Namespace
+    member this.FilePath = file.FullName
+
+type NamespaceSummary(asm:Assembly,ns:string,types:Type seq) = 
+    member this.AssemblyName = System.IO.Path.GetFileName (asm.Location)
+    member this.AssemblyVersion = asm.GetName().Version
+    member this.Namespace = ns
+    member this.Count = types.Count()
+
+type TypeAnalyzer() = 
+    let buildNamespaceSummary asm (ns,types) =
+        new NamespaceSummary(asm, ns, types)
+
+    member this.Analyze(file:FileInfo) = 
+        if file.Exists then 
+            let asm = Assembly.LoadFile file.FullName
+            asm.GetTypes()
+                |> Seq.ofArray
+                |> Seq.sortBy (fun (t:Type) -> t.Namespace+"+"+t.FullName)
+                |> Seq.groupBy (fun (t:Type) -> t.Namespace) 
+                |> Seq.map (buildNamespaceSummary asm)
+        else
+            raise (new InvalidOperationException(String.Format("Could not find '{0}'.", file.FullName)))
+
---
+++ Selanger.Tests/TypeAnalyzerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using ApprovalTests;
+using NUnit.Framework;
+using Selanger.Core;
+
+namespace Selanger.Tests
+{
+    [TestFixture]
+    public class TypeAnalyzerTests
+    {
+        [Test]
+        public void Throws_if_file_not_found()
+        {
+            var asm = @".\i.do.not.exist.dll";
+            var file = new FileInfo(asm);
+            var reporter = new TypeAnalyzer();
+
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+                {
+                    reporter.Analyze(file);
+                });
+
+            Assert.AreEqual("Could not find", ex.Message.Substring(0,14));
+        }
+
+        [Test]
+        public void CanGenerateTypeReport()
+        {
+            var asm = @".\nunit.framework.dll";
+            var file = new FileInfo(asm);
+            var reporter = new TypeAnalyzer();
+            var result = reporter.Analyze(file);
+            Approvals.VerifyAll(result, x => String.Format("{0},{1},{2},{3}", 
+                                                            x.AssemblyName, 
+                                                            x.AssemblyVersion,
+                                                            x.Namespace, 
+                                                            x.Count
+                                                           ));
+        }
+    }
+}
