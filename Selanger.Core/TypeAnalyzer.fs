1b7dbbda03c52c2ac29f3f0d128b6682fcb84d75
David Alpert <david@spinthemoose.com>
2014-02-15 23:23:47 -0600

    now parses and joins results of several assemblies together

--- Selanger.Core/TypeAnalyzer.fs
+++ Selanger.Core/TypeAnalyzer.fs
@@ -17,17 +17,17 @@ type NamespaceSummary(asm:Assembly,ns:string,types:Type seq) =
     member this.Count = types.Count()
 
 type TypeAnalyzer() = 
-    let buildNamespaceSummary asm (ns,types) =
-        new NamespaceSummary(asm, ns, types)
+    let buildNamespaceSummary ((asm,ns),types:Type seq) =
+        let t = types.FirstOrDefault()
+        new NamespaceSummary(t.Assembly, ns, types)
 
-    member this.Analyze(file:FileInfo) = 
-        if file.Exists then 
-            let asm = Assembly.LoadFile file.FullName
-            asm.GetTypes()
-                |> Seq.ofArray
-                |> Seq.sortBy (fun (t:Type) -> t.Namespace+"+"+t.FullName)
-                |> Seq.groupBy (fun (t:Type) -> t.Namespace) 
-                |> Seq.map (buildNamespaceSummary asm)
-        else
-            raise (new InvalidOperationException(String.Format("Could not find '{0}'.", file.FullName)))
+    member this.Analyze([<ParamArray>] files:FileInfo[]) = 
+        files |> Seq.where (fun(f) -> f.Exists) 
+              |> Seq.map (fun(f) -> Assembly.LoadFile f.FullName)
+              |> Seq.collect (fun(a) -> a.GetTypes()) 
+              |> Seq.sortBy (fun (t:Type) -> t.Assembly.Location+"+"+t.Namespace+"+"+t.FullName)
+              |> Seq.groupBy (fun(t:Type) -> t.Assembly)
+              |> Seq.collect (fun(asm,types) -> types |> Seq.groupBy (fun(t) -> (asm,t.Namespace)))
+              |> Seq.map buildNamespaceSummary
+        //| 0 -> raise (new InvalidOperationException(String.Format("Could not find '{0}'.", file.FullName)))
 
--- Selanger.Tests/TypeAnalyzerTests.cs
+++ Selanger.Tests/TypeAnalyzerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using ApprovalTests;
 using NUnit.Framework;
 using Selanger.Core;
@@ -9,21 +10,6 @@ namespace Selanger.Tests
     [TestFixture]
     public class TypeAnalyzerTests
     {
-        [Test]
-        public void Throws_if_file_not_found()
-        {
-            var asm = @".\i.do.not.exist.dll";
-            var file = new FileInfo(asm);
-            var reporter = new TypeAnalyzer();
-
-            var ex = Assert.Throws<InvalidOperationException>(() =>
-                {
-                    reporter.Analyze(file);
-                });
-
-            Assert.AreEqual("Could not find", ex.Message.Substring(0,14));
-        }
-
         [Test]
         public void CanGenerateTypeReport()
         {
@@ -36,6 +22,26 @@ namespace Selanger.Tests
                                                             x.AssemblyVersion,
                                                             x.Namespace, 
                                                             x.Count
+                                                           ));
+        }
+
+        [Test]
+        public void CanGenerateTypeReportForListOfAssemblies()
+        {
+            var files = Directory.GetFiles(".", "*.dll")
+                                .Take(3)
+                                .Select(f => new FileInfo(f))
+                                .ToArray();
+
+            var reporter = new TypeAnalyzer();
+
+            var result = reporter.Analyze(files);
+
+            Approvals.VerifyAll(result, x => String.Format("{0},{1},{2},{3}", 
+                                                            x.Namespace, 
+                                                            x.Count,
+                                                            x.AssemblyName, 
+                                                            x.AssemblyVersion
                                                            ));
         }
     }
